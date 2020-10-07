00ff2099666e7760ba8d7565fd44a193a406fd97
Patricia <patriciafaustinobr@gmail.com>
2020-10-06 22:26:30 -0300

    Exercicio para calcular o valor total que se tem no estoque, utilizei o metodo ToString da classe Object

---
+++ produtoEmEstoque/produtoEmEstoque/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace produtoEmEstoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Qtdd;
+
+        public double ValorEmEstoque()
+        {
+            return Preco * Qtdd;
+        }
+        public void Adicionar(int quantidade)
+        {
+            Qtdd += quantidade;
+        }
+
+        public void Remover(int quantidade)
+        {
+            Qtdd -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome +
+                ", $ " +
+                Preco.ToString("F2", CultureInfo.InvariantCulture) +
+                ", " +
+                Qtdd +
+                " unidades, Total: " +
+                ValorEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ produtoEmEstoque/produtoEmEstoque/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace produtoEmEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Qtdd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            p.Adicionar(quantidade);
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            p.Remover(quantidade);
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+
+        }
+    }
+}
