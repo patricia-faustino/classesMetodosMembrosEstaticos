55e202bc4767bf5c5261d5c62d3326b6051ea9d7
Patricia <patriciafaustinobr@gmail.com>
2020-10-06 21:08:33 -0300

    Exercicio para calcular a area de dois triangulos e informar quais deles é o maior

---
+++ areaTriangulo/areaTriangulo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace areaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            double x1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double x2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double x3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pX = (x1 + x2 + x3) / 2;
+            double aX = Math.Sqrt(pX * (pX- x1) * (pX - x2) * (pX - x3));
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            double y1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double y2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double y3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pY = (y1 + y2 + y3) / 2;
+            double aY = Math.Sqrt(pY * (pY - y1) * (pY - y2) * (pY - y3));
+
+            Console.WriteLine("Área de X: " + aX.ToString("F3",CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y: " + aY.ToString("F3",CultureInfo.InvariantCulture));
+
+            if (aX > aY)
+            {
+                Console.WriteLine("Maior área : X");
+            } else if (aY > aX)
+            {
+                Console.WriteLine("Maior área : Y");
+            }
+            else
+            {
+                Console.WriteLine("As áreas são iguais");
+            }
+
+
+
+
+
+
+        }
+    }
+}
