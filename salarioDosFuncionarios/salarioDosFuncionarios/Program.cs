e0c7b6718c8f230d870758fe99f6a9337120fb61
Patricia <patriciafaustinobr@gmail.com>
2020-10-06 21:51:17 -0300

    Exercicio utilizando calculo do salario medio de dois funcionarios

---
+++ salarioDosFuncionarios/salarioDosFuncionarios/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace salarioDosFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1, f2;
+            f1 = new Funcionario();
+            f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Pessoa: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Pessoa: ");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMedio = (f1.Salario + f2.Salario) / 2;
+            Console.WriteLine("Salário Médio: " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
