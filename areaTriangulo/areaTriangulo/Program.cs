bbd0ac71f4c95be1422a96bca46650b1a835b3f9
Patricia <patriciafaustinobr@gmail.com>
2020-10-06 21:29:13 -0300

    Exercicio para encontrar as áreas e mostrar qual a maior, com uso de classes

--- areaTriangulo/areaTriangulo/Program.cs
+++ areaTriangulo/areaTriangulo/Program.cs
@@ -7,19 +7,29 @@ namespace areaTriangulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entre com as medidas do triângulo X:");
-            double x1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double x2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double x3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double pX = (x1 + x2 + x3) / 2;
-            double aX = Math.Sqrt(pX * (pX- x1) * (pX - x2) * (pX - x3));
 
-            Console.WriteLine("Entre com as medidas do triângulo Y:");
-            double y1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double y2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double y3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double pY = (y1 + y2 + y3) / 2;
-            double aY = Math.Sqrt(pY * (pY - y1) * (pY - y2) * (pY - y3));
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2;
+            double aX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2;
+            double aY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+
+
 
             Console.WriteLine("Área de X: " + aX.ToString("F3",CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y: " + aY.ToString("F3",CultureInfo.InvariantCulture));
